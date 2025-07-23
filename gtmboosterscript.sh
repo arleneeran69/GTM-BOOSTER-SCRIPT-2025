711c1d8f5db1f5240e70377ec78d74abaccb9e00
arleneeran69 <arleneeran69@gmail.com>
2025-07-24 00:11:16 +0800

    Update gtmboosterscript.sh

--- gtmboosterscript.sh
+++ gtmboosterscript.sh
@@ -1,48 +1,35 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-## GTM Booster Menu v1.0
-## Clean interactive DNS/NS/Gateway Manager
-## Author: GeoDevz69
+# === GTM Booster Menu ===
+clear
+echo "=== GTM Booster Configuration Menu ==="
+echo "1) Add DNS (Edit dns.txt)"
+echo "2) Add NS (Edit ns.txt)"
+echo "3) Add Gateway (Edit gateway.txt)"
+echo "0) Exit"
+echo ""
 
-DNS_FILE="$HOME/dnstt/dns.txt"
-NS_FILE="$HOME/dnstt/ns.txt"
-GATEWAY_FILE="$HOME/dnstt/gateway.txt"
+read -p "Choose an option [0-3]: " choice
 
-mkdir -p "$HOME/dnstt"
-
-show_menu() {
-  clear
-  echo -e "\e[1;36m=== GTM Booster Configuration Menu ===\e[0m"
-  echo "1) Add DNS (Edit dns.txt)"
-  echo "2) Add NS (Edit ns.txt)"
-  echo "3) Add Gateway (Edit gateway.txt)"
-  echo "0) Exit"
-  echo -n -e "\nChoose an option [0-3]: "
-  read -r choice
-
-  case "$choice" in
-    1)
-      echo -e "\nOpening DNS list in nano..."
-      nano "$DNS_FILE"
-      ;;
-    2)
-      echo -e "\nOpening NS list in nano..."
-      nano "$NS_FILE"
-      ;;
-    3)
-      echo -e "\nOpening Gateway list in nano..."
-      nano "$GATEWAY_FILE"
-      ;;
-    0)
-      echo -e "\nGoodbye!"
-      exit 0
-      ;;
-    *)
-      echo -e "\nInvalid option. Try again."
-      sleep 1
-      ;;
-  esac
-}
-
-# Run only once
-show_menu
+case "$choice" in
+  1)
+    echo "Opening dns.txt..."
+    nano dns.txt
+    ;;
+  2)
+    echo "Opening ns.txt..."
+    nano ns.txt
+    ;;
+  3)
+    echo "Opening gateway.txt..."
+    nano gateway.txt
+    ;;
+  0)
+    echo "Exiting..."
+    exit 0
+    ;;
+  *)
+    echo "Invalid option. Exiting..."
+    exit 1
+    ;;
+esac
